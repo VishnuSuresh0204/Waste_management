e2036bec44e070ca95b90b0a73e8d4d72eeb218f
Vishnu Suresh <vish3eyedman.all@gmail.com>
2026-06-18 17:21:27 +0530

    Add Login model: custom user authentication with user type tracking

--- waste/myapp/models.py
+++ waste/myapp/models.py
@@ -1,9 +1,21 @@
+
 from django.db import models
-from django.contrib.auth.models import AbstractUser 
+from django.contrib.auth.models import AbstractUser
 
 
-class Login(AbstractUser): 
-  userType = models.CharField( max_length=50 )
-  viewPass = models.CharField( max_length=100, null=True, blank=True )
-  
-  def __str__(self): return self.username
+# ---------------- LOGIN ---------------- #
+
+class Login(AbstractUser):
+    userType = models.CharField(
+        max_length=50
+    )  
+
+    viewPass = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
+
